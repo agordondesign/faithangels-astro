b526699e0f35ae4aeb49c4acb710466f0432d797
Yahya Gordon <andre@agordondesign.com>
2025-12-08 18:40:14 -0500

    minor tweaks

--- .vercel/output/_functions/chunks/page-ssr_C-pp_xb2.mjs
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-
-const sanityClient = createClient(
-            {"apiVersion":"2025-08-28","projectId":"wwfac1ax","dataset":"production","useCdn":false}
-          );
-
-globalThis.sanityClient = sanityClient;
-
-export { sanityClient as s };
